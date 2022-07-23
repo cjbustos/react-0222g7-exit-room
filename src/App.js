d347f24f2f9e8daf1ea7fcb7e1ca08117666d368
cjbustos <bustos_carlos_j@hotmail.com>
2022-07-23 13:43:55 -0300

    Refactor navbar branch only with essential

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import logo from './img/logo.png'
 import Navbar from './components/Navbar';
 import Image from './components/Image';
 import Button from './components/Button'
-import Ad from './components/Ad';
-//import CustomModal from './components/CustomModal'
 
 function App() {
 
@@ -16,7 +14,7 @@ function App() {
     { id: 5, label: 'Contacto', subcategory: false, path: 'contacto' }
   ]
 
-  /* const btnStyle = {
+  const btnStyle = {
     width: 180,
     height: 42,
     backgroundColor: '#0C9D9C',
@@ -24,45 +22,19 @@ function App() {
     borderRadius: 10,
     fontFamily: 'Rationale',
     fontSize: 20
-  } */
+  }
 
   return (
     <div className='App'>
       <header>
-        <Ad />
         <a href='/'><Image path={logo} width={115} height={110} position={'left'} /></a>
         <Navbar navContent={navItems} />
       </header>
       <main>
         <div className='container'>
-          <h1>¿PODES ESCAPAR EN 60 MINUTOS?</h1>
-          <h2>Desafiar tus cinco sentidos es clave</h2>
-          <Button onClick={(e) => { console.log(e) }}>LEER MAS</Button>
+          <Button style={btnStyle} onClick={() => { console.log('Click!') }}>RESERVAR SALA</Button>
         </div>
       </main>
-      <br />
-      <div className='container-steps'>
-        <h1>CARDS</h1>
-      </div>
-      <br />
-      <div className='container-who-play'>
-        <h1>¿QUIENES PUEDEN PARTICIPAR?</h1>
-      </div>
-      <br />
-      <h2 id='episode'>Salas</h2>
-      <section>
-      </section>
-      <h2 id='cities'>Sucursales</h2>
-      <section></section>
-      <h2 id='precios'>Precios</h2>
-      <section></section>
-      <h2 id='productos'>Productos</h2>
-      <section>
-      </section>
-      <h2 id='contacto'>Contacto</h2>
-      <section></section>
-      <br />
-      <footer>Footer</footer>
     </div>
   );
 }
