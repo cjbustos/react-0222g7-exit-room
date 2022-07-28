42235997c4da6d31f36fa209323b44ac6da5480c
cjbustos <bustos_carlos_j@hotmail.com>
2022-07-28 17:37:18 -0300

    Minimal changes

--- src/components/Button/button.styled.js
+++ src/components/Button/button.styled.js
@@ -2,13 +2,13 @@ import styled from "@emotion/styled";
 
 //Creamos un componente con estilos para distribuir las responsabilidades
 export const ButtonWithStyle = styled.button`
-    width: ${({ size }) => size === 'normal' ? '180px' : '90px'};
-    height: ${({ size }) => size === 'normal' ? '42px' : '21px'};
+    width: ${({ size }) => size === 'small' ? '90px' : '180px'};
+    height: ${({ size }) => size === 'small' ? '21px' : '42px'};
     background-color: #0C9D9C;
     color: '#FFFFFF';
     border-radius: 10px;
     font-family: 'Rationale';
-    font-size: ${({ size }) => size === 'normal' ? '20px' : '10px'};`
-    
+    font-size: ${({ size }) => size === 'small' ? '12px' : '22px'};`
+
 export const Text = styled.h1``
 
