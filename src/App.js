9f969f05b6c4423e936afdf62dea615039fe8633
cjbustos <bustos_carlos_j@hotmail.com>
2022-08-08 17:16:25 -0300

    master ready with changes

--- src/App.js
+++ src/App.js
@@ -29,7 +29,6 @@ import { createContext, useState } from "react";
 export const ShopCartContext = createContext()
 
 function App() {
-
   const navItems = [
     {
       id: 1,
@@ -54,174 +53,176 @@ function App() {
   const counter = () => setCount(count = count + 1);
 
   return (
-    <div className="App">
-      {/* NAVBAR */}
+    <ShopCartContext.Provider value={{ count, counter }}>
+      <div className="App">
+        {/* NAVBAR */}
 
-      <header>
-        <a href="/">
-          <Image path={logo} width={115} height={110} position={"left"} />
-        </a>
-        <Navbar data={navItems} />
-      </header>
+        <header>
+          <a href="/">
+            <Image path={logo} width={115} height={110} position={"left"} />
+          </a>
+          <Navbar data={navItems} />
+        </header>
 
-      {/* MAIN */}
+        {/* MAIN */}
 
-      <main>
-        <div className="container">
-          <h1>¿PODES ESCAPAR EN 60 MINUTOS?</h1>
-          <h2>Desafiar tus cinco sentidos es clave</h2>
-          <Button style={style} onClick={() => alert("Click!")}>
-            RESERVAR SALA
-          </Button>
+        <main>
+          <div className="container">
+            <h1>¿PODES ESCAPAR EN 60 MINUTOS?</h1>
+            <h2>Desafiar tus cinco sentidos es clave</h2>
+            <CustomModal label={'RESERVAR SALA'}>
+              <h3>Texto dentro del Modal</h3>
+            </CustomModal>
+          </div>
+        </main>
+
+        {/* BOXES */}
+
+        <div className="container-boxes">
+
+          <div className="name-boxes">
+            <p>Busca tu sucursal</p>
+            <p>Elegi tu sala</p>
+            <p>Reserva turno</p>
+            <p>Arma tu equipo</p>
+            <p>Podras escapar?</p>
+          </div>
+
+          <div className="icon-boxes">
+            <BsSearch />
+            <BsHandIndexThumb />
+            <BsBookmarkCheck />
+            <FaPeopleArrows />
+            <GiExitDoor />
+
+          </div>
         </div>
-      </main>
 
-      {/* BOXES */}
+        {/* SALAS */}
 
-      <div className="container-boxes">
+        <section id="episode">
 
-        <div className="name-boxes">
-          <p>Busca tu sucursal</p>
-          <p>Elegi tu sala</p>
-          <p>Reserva turno</p>
-          <p>Arma tu equipo</p>
-          <p>Podras escapar?</p>
+          <div className="container-episodes">
+            <h2 className="text-title">SALAS</h2>
+            <h3 className="text-undertitle">60 MINUTOS PARA ESCAPAR</h3>
+            <p className="text-description">
+              Dentro de la sala el tiempo vuela, no te duermas! Todo puede ser una
+              pista.
+            </p>
+          </div>
+
+          <div className="cards-container">
+            <Card
+              nombre="ALICIA´S ROOM"
+              imagen="alice"
+              texto="Caiste un mundo inimaginable, un mundo de fantasia. Podrás encontrar la salida?"
+            />
+            <Card
+              nombre="SCHRöDINGER ROOM"
+              imagen="schrödinger"
+              texto="Famoso experimento mental. Podrás liberte antes de convertirte el gato en la bolsa?"
+            />
+            <Card
+              nombre="STRANGER ROOM"
+              imagen="stranger"
+              texto="The upside down is here! Podrás escapar de la maldad que se avecina?"
+            />
+          </div>
+
+        </section>
+
+        {/* PARTICIPANTES */}
+
+        <div className="container-who-play">
+
+          <h1>¿QUIENES PUEDEN PARTICIPAR?</h1>
+
+          <div className="icons-participants">
+            <RiHome4Line />
+            <RiServiceLine />
+            <MdWork />
+            <BsFillPeopleFill />
+          </div>
+
+          <div className="name-participants">
+            <p>Colegios</p>
+            <p>Familia</p>
+            <p>Empresas</p>
+            <p>Amigos</p>
+          </div>
+
         </div>
 
-        <div className="icon-boxes">
-          <BsSearch />
-          <BsHandIndexThumb />
-          <BsBookmarkCheck />
-          <FaPeopleArrows />
-          <GiExitDoor />
-         
+        {/* REDES */}
+
+        <div className="container-social-media">
+
+          <h3 className="text-undertitle">Seguinos en nuestras redes</h3>
+
+          <div className="icons-redes">
+            <BsFacebook />
+            <FaInstagramSquare id="instagram" />
+            <AiFillTwitterCircle />
+            <FaFacebookMessenger />
+            <BsTelegram />
+          </div>
+
         </div>
+
+        {/* SUCURSALES */}
+
+        <section id="cities">
+
+          <div className="container-shops">
+            <h2 className="text-title">SUCURSALES</h2>
+            <h3 className="text-undertitle">Busca tu sucursal mas cercana</h3>
+          </div>
+
+          <div className="cards-container">
+            <Card nombre="San Telmo" imagen="san-telmo" texto="DIRECCION" />
+            <Card
+              nombre="Vicente Lopez"
+              imagen="vicente-lopez"
+              texto="DIRECCION"
+            />
+            <Card nombre="Palermo" imagen="palermo" texto="DIRECCION" />
+          </div>
+
+        </section>
+
+        {/* PRECIOS */}
+
+        <section id="prices">
+
+          <div className="container-prices">
+            <h2 className="text-title">PRECIOS</h2>
+            <p className="text-description">
+              Los precios varian según la cantidad de jugadores
+            </p>
+          </div>
+
+        </section>
+
+        {/* PRODUCTOS O CARROUSEL */}
+
+        <section id="products"></section>
+
+        {/* FORM */}
+
+        <section id="contact">
+
+          <div className="form-container">
+            <h2 className="text-title">CONTACTO</h2>
+            <Form />
+          </div>
+
+        </section>
+
+        {/* FOOTER */}
+
+        <footer>Footer</footer>
       </div>
-
-      {/* SALAS */}
-
-      <section id="episode">
-
-        <div className="container-episodes">
-          <h2 className="text-title">SALAS</h2>
-          <h3 className="text-undertitle">60 MINUTOS PARA ESCAPAR</h3>
-          <p className="text-description">
-            Dentro de la sala el tiempo vuela, no te duermas! Todo puede ser una
-            pista.
-          </p>
-        </div>
-
-        <div className="cards-container">
-          <Card
-            nombre="ALICIA´S ROOM"
-            imagen="alice"
-            texto="Caiste un mundo inimaginable, un mundo de fantasia. Podrás encontrar la salida?"
-          />
-          <Card
-            nombre="SCHRöDINGER ROOM"
-            imagen="schrödinger"
-            texto="Famoso experimento mental. Podrás liberte antes de convertirte el gato en la bolsa?"
-          />
-          <Card
-            nombre="STRANGER ROOM"
-            imagen="stranger"
-            texto="The upside down is here! Podrás escapar de la maldad que se avecina?"
-          />
-        </div>
-
-      </section>
-
-      {/* PARTICIPANTES */}
-
-      <div className="container-who-play">
-
-        <h1>¿QUIENES PUEDEN PARTICIPAR?</h1>
-
-        <div className="icons-participants">
-          <RiHome4Line />
-          <RiServiceLine />
-          <MdWork />
-          <BsFillPeopleFill />
-        </div>
-
-        <div className="name-participants">
-          <p>Colegios</p>
-          <p>Familia</p>
-          <p>Empresas</p>
-          <p>Amigos</p>
-        </div>
-
-      </div>
-
-      {/* REDES */}
-
-      <div className="container-social-media">
-
-        <h3 className="text-undertitle">Seguinos en nuestras redes</h3>
-
-        <div className="icons-redes">
-          <BsFacebook />
-          <FaInstagramSquare id="instagram" />
-          <AiFillTwitterCircle />
-          <FaFacebookMessenger />
-          <BsTelegram />
-        </div>
-
-      </div>
-
-      {/* SUCURSALES */}
-
-      <section id="cities">
-
-        <div className="container-shops">
-          <h2 className="text-title">SUCURSALES</h2>
-          <h3 className="text-undertitle">Busca tu sucursal mas cercana</h3>
-        </div>
-
-        <div className="cards-container">
-          <Card nombre="San Telmo" imagen="san-telmo" texto="DIRECCION" />
-          <Card
-            nombre="Vicente Lopez"
-            imagen="vicente-lopez"
-            texto="DIRECCION"
-          />
-          <Card nombre="Palermo" imagen="palermo" texto="DIRECCION" />
-        </div>
-
-      </section>
-
-      {/* PRECIOS */}
-
-      <section id="prices">
-
-        <div className="container-prices">
-          <h2 className="text-title">PRECIOS</h2>
-          <p className="text-description">
-            Los precios varian según la cantidad de jugadores
-          </p>
-        </div>
-
-      </section>
-
-      {/* PRODUCTOS O CARROUSEL */}
-
-      <section id="products"></section>
-
-      {/* FORM */}
-
-      <section id="contact">
-
-        <div className="form-container">
-          <h2 className="text-title">CONTACTO</h2>
-          <Form />
-        </div>
-        
-      </section>
-
-      {/* FOOTER */}
-
-      <footer>Footer</footer>
-    </div>
+    </ShopCartContext.Provider>
   );
 }
 
