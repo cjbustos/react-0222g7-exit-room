3496800c06e419d0a602687ce0dbe96a7f965860
cjbustos <bustos_carlos_j@hotmail.com>
2022-09-16 20:57:21 -0300

    changes into footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,17 @@
+import "../styles/Footer.css"
+import { IconContext } from "react-icons"
+import { BsFacebook } from "react-icons/bs"
+import { FiInstagram } from "react-icons/fi"
 
 const Footer = (props) => {
-    return <p>{props.text}</p>
+    return (
+        <div>
+            <IconContext.Provider value={{ className: 'react-icons' }}>
+                <BsFacebook />
+                <FiInstagram />
+            </IconContext.Provider>
+        </div>
+    )
 }
 
 export default Footer
