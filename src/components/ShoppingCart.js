d8659849aed0cdc15f6d05478fc562f6455c5590
cjbustos <bustos_carlos_j@hotmail.com>
2022-09-19 20:17:17 -0300

    icon added and color changes

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,25 +1,21 @@
 import "../styles/ShoppingCart.css"
 import { useContext, useEffect, useState } from "react"
 import { FiShoppingCart } from "react-icons/fi";
+import { RiDeleteBin7Fill } from "react-icons/ri";
 import { BookingContext } from "../contexts/BookingContext";
-import { ShopppingCartContext } from "../contexts/ShoppingCartContext";
-import CustomModal from "./CustomModal";
 
 export default function ShoppingCart() {
 
-    const { orders } = useContext(BookingContext)
-    //const { showOrders, display } = useContext(ShopppingCartContext);
-    const [style, setStyle] = useState('with-zero')
+  const { orders } = useContext(BookingContext)
+  //const { showOrders, display } = useContext(ShopppingCartContext);
+  const [style, setStyle] = useState('with-zero')
 
-    //const arrTest = ["Item1", "Item2", "Item3", "Item4", "Item5"]
+  useEffect(() => {
+    if (orders.length !== 0) setStyle('non-zero')
+  }, [orders, style])
 
-    console.log(orders)
-
-    useEffect(() => {
-        if (orders.length !== 0) setStyle('non-zero')
-    }, [orders, style])
-
-    return (
+  return (
+    <div className={`${style}`}>
       <div className="shop-container">
         <div>
           <li className="dropdown">
@@ -27,12 +23,12 @@ export default function ShoppingCart() {
               <a href={"#".concat("/")}>
                 {<FiShoppingCart className="shop-cart-icon" />}
               </a>
-              {/* <FiShoppingCart className="shop-cart-icon" /> */}
               {
                 <div className="dropdown-content">
-                  {orders.map((e) => (
-                    <a key={e} href={"#".concat("/")}>
+                  {orders.map((e, index) => (
+                    <a key={index} href={"#".concat("/")}>
                       {e.episode}
+                      <RiDeleteBin7Fill color="grey" onClick={() => console.log(e)} />
                     </a>
                   ))}
                 </div>
@@ -40,22 +36,10 @@ export default function ShoppingCart() {
             </ol>
           </li>
         </div>
-        <div>
+        <div className={`${style}`}>
           <div className="shop-number">{orders.length}</div>
         </div>
       </div>
-    );
-}
-
-{/* <div className="shop-cart">
-    <div className={`${style}`}>
-        <div className="shop-number">{count}</div>
-        <FiShoppingCart className="shop-cart-icon" />
     </div>
-</div> */}
-
-//showOrders && <List list={orders} />
-/*<div className={`${style}`}>
-    <div className="shop-number">{orders.length}</div>
-    <li><FiShoppingCart /></li>
-</div>*/
+  );
+}
